cd5cdd4017e09f9ca5c296fbbbbab8f4e3e00492
J-Herika <138695574+J-Herika@users.noreply.github.com>
2025-04-15 16:38:24 +0300

    learned how a bit about awt and how to integrate it with go. build a simple signup / login system

--- FileUploadAPI/go.mod
+++ FileUploadAPI/go.mod
@@ -4,11 +4,13 @@ go 1.24.2
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
 	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/lib/pq v1.1.1 // indirect
 	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+	golang.org/x/crypto v0.37.0 // indirect
 	gorm.io/driver/sqlite v1.5.7 // indirect
 	gorm.io/gorm v1.25.7-0.20240204074919-46816ad31dde // indirect
 )
--- FileUploadAPI/main.go
+++ FileUploadAPI/main.go
@@ -1,48 +1,150 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
+	"net/http"
 
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 type User struct {
-	ID           uint   `gorm:"primaryKey"`
-	Username     string `gorm:"size:100"`
+	ID           uint   `gorm:"primaryKey;autoIncrement"`
+	Username     string `gorm:"size:100;unique"`
 	PasswordHash string
 }
 
-func main() {
+type UserInfo struct {
+	Username string
+	Password string
+}
 
-	db, err := gorm.Open(sqlite.Open("Gorm.db"), &gorm.Config{})
+var DB *gorm.DB
+
+func main() {
+	var err error
+	DB, err = gorm.Open(sqlite.Open("Gorm.db"), &gorm.Config{})
 	if err != nil {
 		log.Fatalln("Couldn't Connect to DataBase: ", err)
+	}
+	errConn := DB.AutoMigrate(&User{})
+	if errConn != nil {
+		log.Fatalln("Couldn't Create DataBase schema: ", errConn)
+	}
+
+	http.HandleFunc("/register", registerHandler)
+	http.HandleFunc("/login", loginHandler)
+	errServer := http.ListenAndServe(":3000", nil)
+	if errServer != nil {
+		fmt.Printf("COULDN'T CONNECT TO SERVER")
+	}
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "THIS METHOD ONLY ACCEPTS POST REQUESTS", http.StatusBadRequest)
 		return
 	}
-	errConn := db.AutoMigrate(&User{})
-	if errConn != nil {
-		log.Fatalln("Couldn't Create DataBase: ", errConn)
 
+	var loginData UserInfo
+	err := json.NewDecoder(r.Body).Decode(&loginData)
+	if err != nil {
+		http.Error(w, "COULD NOT DECODE THE LOGIN USER JSON", http.StatusBadRequest)
+		return
 	}
 
-	// user1 := User{ID: 1, Username: "James Watson", PasswordHash: "fd023ued44sf444ew4r4w328422"}
-	//user2 := User{ID: 2, Username: "Astra Eikon", PasswordHash: "fd023ued44sf444ew4r4w328422"}
-	// user3 := User{ID: 3, Username: "Yami Sukarno", PasswordHash: "fd023ued44sf444ew4r4w328422"}
-
-	// db.Create(&user1)
-	// db.Create(&user2)
-	// db.Create(&user3)
-
-	// Fetching the user again to test
-
-	var fetchedUsers []User
-	result := db.Where("ID <= ?", 3).Find(&fetchedUsers)
-
+	var user User
+	result := DB.Where("Username = ?", loginData.Username).First(&user)
 	if result.Error != nil {
-		fmt.Println("DB Error:", result.Error)
-	} else {
-		fmt.Printf("Fetched Users: %+v\n", fetchedUsers)
+		http.Error(w, "USER NOT FOUND", http.StatusNotFound)
+		return
 	}
+	if !passwordComparer([]byte(user.PasswordHash), loginData.Password) {
+		http.Error(w, "WRONG PASSWORD", http.StatusUnauthorized)
+	}
+
+	claim := jwt.MapClaims{
+		"Username": user.Username,
+		"exp":      time.Now().Add(time.Hour * 1),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
+	strToken, errToken := token.SignedString([]byte("verySecure"))
+	if errToken != nil {
+		log.Printf("COULD NOT STRINGIFY TOKEN")
+		return
+	}
+
+	if errEncode := json.NewEncoder(w).Encode(map[string]string{"Token": strToken}); errEncode != nil {
+		http.Error(w, "COULD NOT FETCH TOKEN", http.StatusInternalServerError)
+	}
+
+}
+
+func registerHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only accepts post methods", http.StatusBadRequest)
+		return
+	}
+
+	var user UserInfo
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, "COULD NOT DECODE JSON", http.StatusBadRequest)
+		return
+	}
+
+	// first we hash the password  ps: never store a password or do anything with it without hashing
+	hashedPass := passwordHasher(user.Password)
+
+	// create a new user "Hard coding it for now"
+	newUser := User{Username: user.Username, PasswordHash: string(hashedPass)}
+
+	// adding the user to the database here ps: here am using a {&} to change the variable it self and not to create a copy
+	if errNewUser := addUser(&newUser); errNewUser != nil {
+		http.Error(w, errNewUser.Error(), http.StatusConflict)
+		return
+	}
+
+	// testing purpose
+	w.Header().Set("Content-Type", "application/json")
+	errEncode := json.NewEncoder(w).Encode(&newUser)
+	if errEncode != nil {
+		http.Error(w, "Could NOT SEND USER INFO", http.StatusInternalServerError)
+		return
+	}
+}
+
+// here i am using {*} to access the actual user data instead of creating a copy.
+func addUser(user *User) error {
+	result := DB.Create(user)
+	if result.Error != nil {
+		return errors.New("USER ALREADY REGISTERED")
+	}
+	return nil
+}
+
+func passwordHasher(password string) []byte {
+
+	hashedPass, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil
+	}
+	return hashedPass
+}
+
+func passwordComparer(hashed []byte, password string) bool {
+	result := bcrypt.CompareHashAndPassword([]byte(hashed), []byte(password))
+	if result == nil {
+		log.Printf("COULD NOT ENCRYPT DATA")
+		return false
+	}
+
+	return true
 }
